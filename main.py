aa6b366e90f5bba805b16f985498a27bc3d29415
emperorsixpacks <andrewdvd10@gmail.com>
2025-02-19 22:58:31 +0100

    mod: stable

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+import os
+from dotenv import load_dotenv
 from payroll.app import app
 from payroll.models import db
 
-
+load_dotenv(".env")
+DEBUG= bool(int(os.environ.get("DEBUG", 1)))
 def run():
     with app.app_context():
         db.create_all()  # Ensure the database is created
-    app.run(debug=True)
+    app.run(debug=DEBUG)
 
 
 if __name__ == "__main__":
